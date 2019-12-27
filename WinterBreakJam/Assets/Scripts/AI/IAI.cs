c0562c276b4bedc4904247e49dca5003bd7becb2
Brian Herman <herman.brian.m@gmail.com>
2019-12-26 22:58:00 -0600

    Initial Push 1/-

---
+++ WinterBreakJam/Assets/Scripts/AI/IAI.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IAI
+{
+    void AITick();
+    void PathFind();
+}
---
+++ WinterBreakJam/Assets/Scripts/AI/NPC.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Pathfinding;
+using UnityEngine;
+
+public class NPC : AIPath, ITargetable, IAI
+{
+    bool currentlySelected = false; //Testing base functionality
+    [SerializeField] protected Transform m_transformTarget = null;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        //TODO: Remove this, temp line. 
+        //m_transformTarget = GameObject.Find("TEST_TARGET").transform;//null;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        AITick();
+    }
+
+    public void SetTargetTransform(Transform target = null)
+    {
+        m_transformTarget = target;
+    }
+
+    public void SetTargetPosition(Vector3 target)
+    {
+        destination = target;
+    }
+
+    #region Targetable_Functionality
+    public void OnHit()
+    {
+        //Handle Hit Functionality
+    }
+
+    public void OnSelectBegin()
+    {
+        currentlySelected = true;
+    }
+
+    public void OnSelectEnd()
+    {
+        currentlySelected = false;
+    }
+    #endregion
+
+    #region AI_Functionality
+    public void AITick()
+    {
+        if(m_transformTarget != null)
+        {
+            destination = m_transformTarget.position;
+        }
+    }
+
+    public void PathFind()
+    {
+
+    }
+    #endregion
+}
